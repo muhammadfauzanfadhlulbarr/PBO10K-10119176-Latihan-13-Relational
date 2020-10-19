351487496a8d422e7ff4752125ee83ec6363558a
Muhammad Fauzan <muhammadfauzanfadhlulbarr@gmail.com>
2020-10-19 10:59:02 +0700

    commit

---
+++ Latihan13.java
@@ -0,0 +1,12 @@
+public class Latihan13 {
+   public static void main(String[] args) {
+       int bill = 10;
+       int bil2 = 20;
+        System.out.println("a == b = " + (bill == bil2));
+        System.out.println("a != b = " + (bill != bil2));
+        System.out.println("a > b = " + (bill > bil2));
+        System.out.println("b < a = " + (bill < bil2));
+        System.out.println("b >= a = " + (bil2 >= bill));
+        System.out.println("c <= a = " + (bil2 <= bill));
+   } 
+}
